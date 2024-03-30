8f08cccc63e98dbddd819a0a2b40c61c8da168e2
TheDingodile <jakob@3esser.dk>
2024-03-30 18:50:21 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-mkdir -p outputs/test_real_data_64/Markdown
-bsub -o "outputs/test_real_data_64/Markdown/test_real_data_64_0.md" -J "test_real_data_64_0" -env MYARGS="-name test_real_data_64-0 -time 82800 -data_name mimic_iv/train/data.json -labels_name mimic_iv/train/label.json -answer_name mimic_iv/train/answer.json -model_type t5 -batch_size 64 -ID 0" < submit_gpu_v32.sh
+mkdir -p outputs/test_real_data_16/Markdown
+bsub -o "outputs/test_real_data_16/Markdown/test_real_data_16_0.md" -J "test_real_data_16_0" -env MYARGS="-name test_real_data_16-0 -time 82800 -data_name mimic_iv/train/data.json -labels_name mimic_iv/train/label.json -answer_name mimic_iv/train/answer.json -model_type t5 -batch_size 16 -ID 0" < submit_gpu_v32.sh
+mkdir -p outputs/test_real_data_32/Markdown
+bsub -o "outputs/test_real_data_32/Markdown/test_real_data_32_0.md" -J "test_real_data_32_0" -env MYARGS="-name test_real_data_32-0 -time 82800 -data_name mimic_iv/train/data.json -labels_name mimic_iv/train/label.json -answer_name mimic_iv/train/answer.json -model_type t5 -batch_size 32 -ID 0" < submit_gpu_v32.sh
--- generate.py
+++ generate.py
@@ -1,15 +1,15 @@
 from main import Defaults, GPU
 
-# Defaults(name="test_real_data", 
-#          GPU=GPU.v32, 
-#          batch_size=128, 
-#          model_type="t5",
-#          data_name="mimic_iv/train/data.json",
-#          labels_name="mimic_iv/train/label.json",
-#          answer_name="mimic_iv/train/answer.json")
-Defaults(name="test_real_data_64", 
+Defaults(name="test_real_data_16", 
          GPU=GPU.v32, 
-         batch_size=64, 
+         batch_size=16, 
+         model_type="t5",
+         data_name="mimic_iv/train/data.json",
+         labels_name="mimic_iv/train/label.json",
+         answer_name="mimic_iv/train/answer.json")
+Defaults(name="test_real_data_32", 
+         GPU=GPU.v32, 
+         batch_size=32, 
          model_type="t5",
          data_name="mimic_iv/train/data.json",
          labels_name="mimic_iv/train/label.json",
