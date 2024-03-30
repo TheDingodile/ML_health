cdf4e8571a59130fcfbe44784347a0d39d95bc8e
TheDingodile <jakob@3esser.dk>
2024-03-30 23:28:16 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/tester16b/Markdown
-bsub -o "outputs/tester16b/Markdown/tester16b_0.md" -J "tester16b_0" -env MYARGS="-name tester16b-0 -time 82800 -data_name mimic_iv/train/data.json -labels_name mimic_iv/train/label.json -answer_name mimic_iv/train/answer.json -model_type t5 -batch_size 16 -ID 0" < submit_gpu_v32.sh
-mkdir -p outputs/tester32b/Markdown
-bsub -o "outputs/tester32b/Markdown/tester32b_0.md" -J "tester32b_0" -env MYARGS="-name tester32b-0 -time 82800 -data_name mimic_iv/train/data.json -labels_name mimic_iv/train/label.json -answer_name mimic_iv/train/answer.json -model_type t5 -batch_size 32 -ID 0" < submit_gpu_a80.sh
+mkdir -p outputs/tester16c/Markdown
+bsub -o "outputs/tester16c/Markdown/tester16c_0.md" -J "tester16c_0" -env MYARGS="-name tester16c-0 -time 82800 -data_name mimic_iv/train/data.json -labels_name mimic_iv/train/label.json -answer_name mimic_iv/train/answer.json -model_type t5 -batch_size 16 -eval_fraction 10 -ID 0" < submit_gpu_v32.sh
--- generate.py
+++ generate.py
@@ -9,18 +9,26 @@ from main import Defaults, GPU
 #          answer_name="mimic_iv/train/answer.json")
 
 
-Defaults(name="tester16b", 
+# Defaults(name="tester16b", 
+#          GPU=GPU.v32, 
+#          batch_size=16, 
+#          model_type="t5",
+#          data_name="mimic_iv/train/data.json",  
+#          labels_name="mimic_iv/train/label.json",
+#          answer_name="mimic_iv/train/answer.json")
+
+# Defaults(name="tester32b", 
+#          GPU=GPU.a80, 
+#          batch_size=32, 
+#          model_type="t5",
+#          data_name="mimic_iv/train/data.json",
+#          labels_name="mimic_iv/train/label.json",
+#          answer_name="mimic_iv/train/answer.json")
+
+Defaults(name="tester16c", 
          GPU=GPU.v32, 
          batch_size=16, 
          model_type="t5",
-         data_name="mimic_iv/train/data.json",
-         labels_name="mimic_iv/train/label.json",
-         answer_name="mimic_iv/train/answer.json")
-
-Defaults(name="tester32b", 
-         GPU=GPU.a80, 
-         batch_size=32, 
-         model_type="t5",
-         data_name="mimic_iv/train/data.json",
+         data_name="mimic_iv/train/data.json",  
          labels_name="mimic_iv/train/label.json",
          answer_name="mimic_iv/train/answer.json")
