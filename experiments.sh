bd256f8287344255470ac5247aa0d97bfc4a384c
TheDingodile <jakob@3esser.dk>
2024-03-30 18:40:10 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/test/Markdown
-bsub -o "outputs/test/Markdown/test_0.md" -J "test_0" -env MYARGS="-name test-0 -time 82800 -data_name sample_data/train/data.json -labels_name sample_data/train/label.json -answer_name sample_data/train/answer.json -model_type t5 -batch_size 64 -ID 0" < submit_gpu_v32.sh
+mkdir -p outputs/test_real_data/Markdown
+bsub -o "outputs/test_real_data/Markdown/test_real_data_0.md" -J "test_real_data_0" -env MYARGS="-name test_real_data-0 -time 82800 -data_name mimic_iv/train/data.json -labels_name mimic_iv/train/label.json -answer_name mimic_iv/train/answer.json -model_type t5 -batch_size 128 -ID 0" < submit_gpu_v32.sh
--- generate.py
+++ generate.py
@@ -1,3 +1,9 @@
 from main import Defaults, GPU
 
-Defaults(name="test", GPU=GPU.v32, batch_size=64, model_type="t5")
+Defaults(name="test_real_data", 
+         GPU=GPU.v32, 
+         batch_size=128, 
+         model_type="t5",
+         data_name="mimic_iv/train/data.json",
+         labels_name="mimic_iv/train/label.json",
+         answer_name="mimic_iv/train/answer.json")
