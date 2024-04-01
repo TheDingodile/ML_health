bb516bf09a5df1a8483068371f27d5c347ecc821
TheDingodile <jakob@3esser.dk>
2024-04-01 14:40:52 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/Add_information/Markdown
-bsub -o "outputs/Add_information/Markdown/Add_information_0.md" -J "Add_information_0" -env MYARGS="-name Add_information-0 -time 82800 -data_name mimic_iv/train/data.json -labels_name mimic_iv/train/label.json -answer_name mimic_iv/train/answer.json -prediction_name mimic_iv/valid/data.json -model_type t5 -t5_model_name t5-base -batch_size 16 -eval_fraction 25 -null_chance_boundary 1.0 -make_predictions_after 3 -lr 0.001 -max_length_source 1024 -max_length_target 512 -ID 0" < submit_gpu_a80.sh
+mkdir -p outputs/Ensemble_fix/Markdown
+bsub -o "outputs/Ensemble_fix/Markdown/Ensemble_fix_0.md" -J "Ensemble_fix_0" -env MYARGS="-name Ensemble_fix-0 -time 82800 -data_name mimic_iv/train/data.json -labels_name mimic_iv/train/label.json -answer_name mimic_iv/train/answer.json -prediction_name mimic_iv/valid/data.json -model_type t5 -t5_model_name t5-base -batch_size 16 -eval_fraction 50 -null_chance_boundary 1.0 -make_predictions_after 5 -lr 0.001 -max_length_source 512 -max_length_target 512 -ID 0" < submit_gpu_v32.sh
