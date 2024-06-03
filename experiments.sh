af068e7a2910eedec1c9c10abbd166a77807b502
TheDingodile <jakob@3esser.dk>
2024-06-03 16:22:30 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p outputs/project2LLM/Markdown
-bsub -o "outputs/project2LLM/Markdown/project2LLM_0.md" -J "project2LLM_0" -env MYARGS="-name project2LLM-0 -time 82800 -data_name mimic_iv/train/data.json -prediction_name mimic_iv/valid/data.json -model_type t5 -t5_model_name t5-base -batch_size 12 -make_predictions_after 2 -lr 0.001 -max_length_source 512 -max_length_target 512 -append_scheme_info True -give_extra_info True -ID 0" < submit_gpu_v32.sh
+bsub -o "outputs/project2LLM/Markdown/project2LLM_0.md" -J "project2LLM_0" -env MYARGS="-name project2LLM-0 -time 82800 -data_name dataset/mimic_iv_cxr/train/train_data.json -prediction_name dataset/mimic_iv_cxr/valid/valid_data.json -model_type t5 -t5_model_name t5-base -batch_size 12 -make_predictions_after 2 -lr 0.001 -max_length_source 512 -max_length_target 512 -append_scheme_info True -give_extra_info True -ID 0" < submit_gpu_v32.sh
